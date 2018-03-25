8f322330ea185c873cf27e3e2d4c4b3bf3aba892
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-03-25 15:08:25 +0300

    added -s <signal> or --signal=<signal> parameter

--- cli.js
+++ cli.js
@@ -5,7 +5,8 @@ const kill = require('./')
 const args = require('get-them-args')(process.argv.slice(2))
 
 const port = args.port || args.unknown
+const signal = args.signal || args.s || 9 // see kill -l, man kill
 
-kill(port)
-  .then(() => console.log(`Process on port ${port} killed`))
+kill(port, signal)
+  .then(() => console.log(`Process on port ${port} killed with signal ${signal}`))
   .catch(() => console.log(`Could not kill process on port ${port}`))
--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const sh = require('execa').shell
 
-module.exports = function (port) {
+module.exports = function (port, signal) {
   if (!Number.parseInt(port)) {
     return Promise.reject(new Error('Invalid argument provided for port'))
   }
 
-  return sh(`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`)
+  return sh(`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -${signal}`)
 }
